95b957484f4ae25725b0e747640e1137071bda5a
keith <keithlegg23@gmail.com>
2024-11-24 12:16:56 -0800

    who cares

--- builder.py
+++ builder.py
@@ -1,1 +1,43 @@
 #!/usr/bin/python
+
+import struct 
+
+
+
+
+class binary_blob(object):
+    def __init_(self):
+        pass 
+
+    #def insert(self, input, num, position):
+
+    #def remove(self, num, position):
+    
+    #def split(self, num, position):
+
+
+    def int_seq(self, size=1024):
+        bdata = []
+        for b in range(size):
+            bdata.append(b)
+        return bdata  
+
+    def dumpfile(self, bdata, filepath, encoding='int'):
+        # encoding 'utf-8'
+        # arr = bytearray(string, 'utf-8')
+
+        ba = bytearray(bdata)
+ 
+        nf = open(filepath,'wb')
+        if encoding=='int':
+            nf.write(struct.pack('%sB'%len(ba), *ba))
+
+
+
+
+b = binary_blob()
+seq = b.int_seq(100)
+b.dumpfile(seq, 'foo.bin')
+
+
+
